d45738ac7fca3f9aac059cdadfc7a31af20b049d
Grizzlazy <134766687+Grizzlazy@users.noreply.github.com>
2025-04-04 00:26:58 +0700

    3-5-loop

--- Data.py
+++ Data.py
@@ -7,9 +7,9 @@ import time
 import itertools
 import collections
 import os
-number_of_trucks = 2
+number_of_trucks = 1
 number_of_cities = 0
-number_of_drones = 2
+number_of_drones = 1
 truck_speed = 0.5
 drone_speed = 1
 drone_capacity = 4
@@ -27,7 +27,7 @@ B_ratio = 0.7
 C_ratio = 0.1
 delta = 0.3
 alpha = [0.5, 0.3, 0.1]
-theta = 2
+theta = float(os.getenv('THETA'))
 
 
 def read_data(path):
